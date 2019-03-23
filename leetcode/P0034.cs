41081b07d0bd6b5aee5d9e3a15f9972c7a816079
Rick Sladkey <rick.sladkey@gmail.com>
2019-03-23 16:43:36 -0400

    Add solution to problem 34

---
+++ leetcode/P0034.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode0034
+{
+    public class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var beg = Rank(nums, target);
+            var end = Rank(nums, target + 1);
+            if (beg == end) return new[] { -1, -1 };
+            else return new[] { beg, end - 1 };
+        }
+        private int Rank(int[] nums, int val)
+        {
+            var lo = 0;
+            var hi = nums.Length;
+            while (lo < hi)
+            {
+                var mid = lo + (hi - lo) / 2;
+                if (nums[mid] < val) lo = mid + 1;
+                else hi = mid;
+            }
+            return lo;
+        }
+    }
+}
