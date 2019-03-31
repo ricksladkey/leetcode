91f2338f7699c9db736bb606845f4bf4182b3ccb
Rick Sladkey <rick.sladkey@gmail.com>
2019-03-31 00:56:48 -0400

    Add solution to problem 1031

---
+++ leetcode/P1031.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode1031
+{
+    public class Solution {
+        private int[] id;
+        private int[] sz;
+        private int rows;
+        private int cols;
+        public int NumEnclaves(int[][] grid) {
+            rows = grid.Length;
+            if (rows == 0) return 0;
+            cols = grid[0].Length;
+            var n = rows * cols;
+            id = new int[n + 1];
+            sz = new int[n + 1];
+            for (var i = 0; i <= n; i++) id[i] = i;
+            for (var row = 0; row < rows; row++) {
+                for (var col = 0; col < cols; col++) {
+                    var square = grid[row][col];
+                    if (square == 1) {
+                        if (row > 0 && grid[row - 1][col] == 1)
+                            union(getid(row, col), getid(row - 1, col));
+                        if (col > 0 && grid[row][col - 1] == 1)
+                            union(getid(row, col), getid(row, col - 1));
+                        if (row < rows - 1 && grid[row + 1][col] == 1)
+                            union(getid(row, col), getid(row + 1, col));
+                        if (col < cols - 1 && grid[row][col + 1] == 1)
+                            union(getid(row, col), getid(row, col + 1));
+                        if (row == 0 || col == 0 || row == rows - 1 || col == cols - 1)
+                            union(getid(row, col), n);
+                    }
+                }
+            }
+            var cnt = 0;
+            for (var row = 0; row < rows; row++) {
+                for (var col = 0; col < cols; col++) {
+                    var square = grid[row][col];
+                    if (square == 1 && !find(getid(row, col), n)) cnt += 1;
+                }
+            }
+            return cnt;
+        }
+        private int getid(int row, int col) {
+            return row * cols + col;
+        }
+        private int root(int i) {
+            while (i != id[i]) {
+                id[i] = id[id[i]];
+                i = id[i];
+            }
+            return i;
+        }
+        private bool find(int p, int q) {
+            return root(p) == root(q);
+        }
+        private void union(int p, int q) {
+            int i = root(p);
+            int j = root(q);
+            if (i == j) return;
+            if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+            else { id[j] = i; sz[i] += sz[j]; }     }
+    }
+}
