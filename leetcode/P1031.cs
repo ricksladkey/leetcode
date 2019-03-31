66f1ae55fb0532ff40cd5af2e4c9248c266fbaeb
Rick Sladkey <rick.sladkey@gmail.com>
2019-03-31 01:10:20 -0400

    Add commentary

--- leetcode/P1031.cs
+++ leetcode/P1031.cs
@@ -13,27 +13,33 @@ namespace leetcode1031
             rows = grid.Length;
             if (rows == 0) return 0;
             cols = grid[0].Length;
+            
+            // Create Union-Find data structure with one
+            // more node representing the edge of the grid.
             var n = rows * cols;
             id = new int[n + 1];
             sz = new int[n + 1];
             for (var i = 0; i <= n; i++) id[i] = i;
+            
+            // Connect all lands to neighbors and possibly the edge.
             for (var row = 0; row < rows; row++) {
                 for (var col = 0; col < cols; col++) {
                     var square = grid[row][col];
                     if (square == 1) {
-                        if (row > 0 && grid[row - 1][col] == 1)
-                            union(getid(row, col), getid(row - 1, col));
-                        if (col > 0 && grid[row][col - 1] == 1)
-                            union(getid(row, col), getid(row, col - 1));
-                        if (row < rows - 1 && grid[row + 1][col] == 1)
-                            union(getid(row, col), getid(row + 1, col));
-                        if (col < cols - 1 && grid[row][col + 1] == 1)
-                            union(getid(row, col), getid(row, col + 1));
-                        if (row == 0 || col == 0 || row == rows - 1 || col == cols - 1)
-                            union(getid(row, col), n);
+                    
+                        // Check the four neighbors for lands and connect them.
+                        if (row > 0 && grid[row - 1][col] == 1) union(getid(row, col), getid(row - 1, col));
+                        if (col > 0 && grid[row][col - 1] == 1) union(getid(row, col), getid(row, col - 1));
+                        if (row < rows - 1 && grid[row + 1][col] == 1) union(getid(row, col), getid(row + 1, col));
+                        if (col < cols - 1 && grid[row][col + 1] == 1) union(getid(row, col), getid(row, col + 1));
+                        
+                        // Connect border cells with the edge.
+                        if (row == 0 || col == 0 || row == rows - 1 || col == cols - 1) union(getid(row, col), n);
                     }
                 }
             }
+            
+            // Sum all lands not connected to the edge.
             var cnt = 0;
             for (var row = 0; row < rows; row++) {
                 for (var col = 0; col < cols; col++) {
