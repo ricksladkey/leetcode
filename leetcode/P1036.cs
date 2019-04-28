49e3b115fdabd6530327526ad3e41add5cb901ac
Rick Sladkey <rick.sladkey@gmail.com>
2019-04-28 01:05:01 -0400

    First pass at cleaning up escape problem

--- leetcode/P1036.cs
+++ leetcode/P1036.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace leetcode1036
 {
     public class Solution {
+
+        // Union-Find algorithm
         public class UnionFind {
             private int[] id;
             private int[] sz;
@@ -15,7 +17,7 @@ namespace leetcode1036
                 sz = new int[n];
                 for (var i = 0; i < n; i++) id[i] = i;
             }
-            public int Root(int i) {
+            private int Root(int i) {
                 while (i != id[i]) {
                     id[i] = id[id[i]];
                     i = id[i];
@@ -33,52 +35,56 @@ namespace leetcode1036
                 else { id[j] = i; sz[i] += sz[j]; }
             }
         }
-        private int rows;
-        private int cols;
+
+        private int rows, cols;
         private Dictionary<int, int> rowMap, colMap;
         public bool IsEscapePossible(int[][] blocked, int[] source, int[] target) {
-            for (var index = 0; index <= 1; index++) {
-                var max = 0;
-                var ids =
-                    blocked.Select(pair => pair[index])
-                    .Concat(new [] { 0, source[index], target[index], 999999 })
-                    .OrderBy(id => id)
-                    .Distinct()
-                    .ToArray();
-                var map = new Dictionary<int, int>();
-                var next = 0;
-                for (var i = 0; i < ids.Length; i++) {
-                    if (i != 0) next += Math.Min(2, ids[i] - ids[i - 1]);
-                    map.Add(ids[i], next);
-                    max = Math.Max(max, next);
-                }
-                if (index == 0) { rows = max + 1; rowMap = map; }
-                if (index == 1) { cols = max + 1; colMap = map; }
-            }
+
+            // Compress consecutive blank rows and columns.
+            (rows, rowMap) = GetMap(blocked, source, target, 0);
+            (cols, colMap) = GetMap(blocked, source, target, 1);
+
+            // Create a grid from the compressed data.
             var grid = new bool[rows, cols];
             foreach (var pair in blocked) grid[rowMap[pair[0]], colMap[pair[1]]] = true;
+
+            // Connect unblocked squares to each other.
             var uf = new UnionFind(rows * cols);
-            for (var row = 0; row < rows; row++) {
-                for (var col = 0; col < cols; col++) {
-                    if (!grid[row, col]) {
-                        if (row > 0 && !grid[row - 1, col])
-                            uf.Union(GetId(row, col), GetId(row - 1, col));
-                        if (col > 0 && !grid[row, col - 1])
-                            uf.Union(GetId(row, col), GetId(row, col - 1));
-                        if (row < rows - 1 && !grid[row + 1, col])
-                            uf.Union(GetId(row, col), GetId(row + 1, col));
-                        if (col < cols - 1 && !grid[row, col + 1])
-                            uf.Union(GetId(row, col), GetId(row, col + 1));
+            var offs = new[] { (0, 1), (1, 0), (0, -1), (-1, 0) };
+            for (var row = 0; row < rows; row++) for (var col = 0; col < cols; col++) {
+                if (!grid[row, col]) foreach (var off in offs)
+                {
+                    int adjRow = row + off.Item1, adjCol = col + off.Item2;
+                    if (adjRow >= 0 && adjRow < rows && adjCol >= 0 && adjCol < cols)
+                    {
+                        if (!grid[adjRow, adjCol]) uf.Union(GetId(row, col), GetId(adjRow, adjCol));
                     }
                 }
             }
+
+            // Check whether the source is connected to the target.
             return uf.Find(GetMappedId(source), GetMappedId(target));
+        }
+
+        private (int, Dictionary<int, int>) GetMap(int[][] blocked, int[] source, int[] target, int index) {
+            var size = 0;
+            var map = new Dictionary<int, int>();
+            var ids = blocked.Select(pair => pair[index])
+                .Concat(new [] { 0, source[index], target[index], 999999 })
+                .OrderBy(id => id).Distinct().ToArray();
+            var next = 0;
+            for (var i = 0; i < ids.Length; i++) {
+                if (i != 0) next += Math.Min(2, ids[i] - ids[i - 1]);
+                map.Add(ids[i], next);
+                size = Math.Max(size, next + 1);
+            }
+            return (size, map);
         }
         private int GetId(int row, int col) {
             return row * cols + col;
         }
         private int GetMappedId(int[] pair) {
-            return rowMap[pair[0]] * cols + colMap[pair[1]];
+            return GetId(rowMap[pair[0]], colMap[pair[1]]);
         }
     }
 }
